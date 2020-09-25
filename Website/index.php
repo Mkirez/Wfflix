fb4257c4185f5c07d5660179ba22dfe469ad7401
Alex <AlexSteen303@gmail.com>
2020-09-25 17:43:24 +0200

    Login functie Wfflix naar MVC-model

--- Website/index.php
+++ Website/index.php
@@ -3,6 +3,8 @@
 require 'controllers/HomeController.php';
 require 'controllers/AboutController.php';
 require 'controllers/ContactController.php';
+require 'controllers/RegisterController.php';
+require 'controllers/LoginController.php';
 
 require 'core/Router.php';
 
