6781e59e38eec329fdf6c74d1122369e3bfdcdd6
lvvcolin <lorenzo8399@hotmail.com>
2020-10-03 21:15:34 +0200

    search video met jquery zit erin

--- Website/views/course.view.php
+++ Website/views/course.view.php
@@ -8,224 +8,141 @@ include "includes/head.view.php"
 
 <body>
 <?php include "includes/nav.view.php" ?>
-<div class="wrapper">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-12 col-xs-12 col-md-12 col-sm-12 col-lg-12 text-left videos-courses">
-                <div class="course-text">
-                    <h1>html & css</h1>
-                </div>
-                <div class="row">
-                    <div class="col-sm-12 col-xs-12 col-md-4">
-                        <div class="row">
-                            <div class="col-md-12 col-sm-12 col-xs-12">
-                                <div class="inner2">
-                                    <a href="/html_learn">
-                                        <img src="https://i.ytimg.com/vi/pQN-pnXPaVg/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDSHd4tgm81VMHWtFRnQJDAe-K3Bg" style="width: 100%; height: 100%;">
-                                    </a>
-                                    <!-- <iframe  width="560" height="315" src="https://www.youtube.com/embed/ceILacCNzEU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
-
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-12 col-sm-12 col-xs-12 text-left card-text">
-                                        <h2 class="card-title">learn html</h2>
-                                        <p>Lorem Ipsum Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-sm-12 col-xs-12 col-md-4">
-                        <div class="row">
-                            <div class="col-sm-12 col-xs-12 col-md-12">
-                                <div class="inner2">
-
-                                    <a href="/css_learn">
-                                        <img src="https://i.ytimg.com/vi/1PnVor36_40/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAvN_gUkonX_alACtoe7wG14-_L5w" style="width: 100%; height: 100%;">
-                                    </a>
 
 
 
-                                   <!--  <iframe width="560" height="315" src="https://www.youtube.com/embed/1Rs2ND1ryYc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
- -->
-                                </div>
-                                <div class="row">
-                                    <div class="col-sm-12 col-xs-12 col-md-12 text-left card-text">
-                                        <h2 class="card-title">Learn CSS</h2>
-                                        <p>Lorem Ipsum Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
 
-                    <div class=" col-sm-12 col-xs-12 col-md-4">
-                        <div class="row">
-                            <div class="col-sm-12 col-xs-12 col-md-12">
-                                <div class="inner2">
-                                    <a href="/html&css">
-                                        <img src="https://i.ytimg.com/vi/mU6anWqZJcc/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLAC9NE3V6MUSXUt8x29wnVIYhE0LA" style="width: 100%; height: 100%;">
-                                    </a>
 
-                                    <!-- <iframe width="560" height="315" src="https://www.youtube.com/embed/mU6anWqZJcc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
- -->       
-                                </div>
-                                <div class="row">
-                                    <div class="col-sm-12 col-xs-12 col-md-12 text-left card-text">
-                                        <h2 class="card-title">full course html & css</h2>
-                                        <p>Lorem Ipsum Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+<div class="space">
+</div>
+<div class="container">
+    <div class="col-md-12">
+        <div class="form-group has-search">
+            <span class="fa fa-search form-control-feedback"></span>
+            <input type="text" class="form-control" placeholder="Search" id="myinput" type="text"  placeholder="Search..." >
         </div>
-        <div class="row">
-            <div class="col-md-12 text-left videos-courses">
-                <div class="course-text">
-                    <h1>php</h1>
-                </div>
-                <div class="row">
-                    <div class="col-sm-12 col-xs-12 col-md-4">
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="inner2">
-                                    <a href="/phpintro">
-                                        <img src="https://i.ytimg.com/vi/OK_JCtrrv-c/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBaBrtaxkpLhXxVBHE9e70I0saBSA" style="width: 100%; height: 100%;">
-                                    </a>
 
-                                    <!-- <iframe width="560" height="315" src="https://www.youtube.com/embed/XBj_le81sAc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-12 text-left card-text">
-                                        <h2 class="card-title">intro php course</h2>
-                                        <p>Lorem Ipsum Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-12 col-xs-12 col-md-4">
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="inner2">
-                                    <a href="/phpsyntax">
-                                        <img src="https://i.ytimg.com/vi/U10yvfIStx8/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBJOOubzBGJVKM6fv5hWsQp7XEGlw" style="width: 100%; height: 100%;">
-                                    </a>
-
-                                  <!--   <iframe width="560" height="315" src="https://www.youtube.com/embed/U10yvfIStx8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
-
-
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-12 text-left card-text">
-                                        <h2 class="card-title">php basic syntax</h2>
-                                        <p>Lorem Ipsum Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="inner2">
-                                    <a href="/phpinclude">
-                                        <img src="https://i.ytimg.com/vi/fMTvi3Rys-o/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLB0oED0E5fkzmAsZ6eH5yd1-hpyCg" style="width: 100%; height: 100%;">
-                                    </a>
-                                    
-                                    <!-- <iframe width="560" height="315" src="https://www.youtube.com/embed/fMTvi3Rys-o" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
-
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-12 text-left card-text">
-                                        <h2 class="card-title">include files in php</h2>
-                                        <p>Lorem Ipsum Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12 text-left videos-courses">
-                <div class="course-text">
-                    <h1>javascript</h1>
-                </div>
-                <div class="row">
-                    <div class="col-md-4">
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="inner2">
-                                    <a href="/jsfundamentals">
-                                        <img src="https://i.ytimg.com/vi/vEROU2XtPR8/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDTZcrG1hqreOkiGhHxj8_exMY5QA" style="width: 100%; height: 100%;">
-                                    </a>
-                                    
-
-                                    <!-- <iframe width="560" height="315" src="https://www.youtube.com/embed/PigbqPGfIBk" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
-
-
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-12 text-left card-text">
-                                        <h2 class="card-title">javascript fundamentals</h2>
-                                        <p>Lorem Ipsum Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="inner2">
-                                    <a href="/jsvariables">
-                                        <img src="https://i.ytimg.com/vi/edlFjlzxkSI/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDKc0UBXRuI9xhXO4z5sRMTLGGc8A" style="width: 100%; height: 100%;">
-                                    </a>
-
-                                    <!-- <iframe width="560" height="315" src="https://www.youtube.com/embed/CBPUpprXrIA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
-
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-12 text-left card-text">
-                                        <h2 class="card-title">variables</h2>
-                                        <p>Lorem Ipsum Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="inner2">
-                                    <a href="/jsdatatypes">
-                                        <img src="https://i.ytimg.com/vi/O9by2KcR2v4/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBsHijj7g-l_zjU25X2AnabhNt24w" style="width: 100%; height: 100%;">
-                                    </a>
-
-                                    <!-- <iframe width="560" height="315" src="https://www.youtube.com/embed/ztuGhf_TOz4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-12 text-left card-text">
-                                        <h2 class="card-title">datatypes</h2>
-                                        <p>Lorem Ipsum Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 </div>
+
+<div class="wrapper">
+    <div class="container-fluid">
+                
+        <div class="row" id="card">
+            <div class="col-sm-12 col-xs-12 col-md-4 portfolio-thumbnail ">
+                <div class="inner2" id="card">
+                  <a href="/html_learn">
+                        <img src="https://i.ytimg.com/vi/pQN-pnXPaVg/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDSHd4tgm81VMHWtFRnQJDAe-K3Bg" style="width:100%; height: 100%;">
+                    </a>
+                    <h2 class="text-white"><b>html intro</b></h2> 
+                    <p class="text-white">lorem ipsum</p>        
+                </div>
+            </div>
+
+            <div class="col-sm-12 col-xs-12 col-md-4 portfolio-thumbnail ">
+                <div class="inner2" id="card">
+                  <a href="/css_learn">
+                    <img src="https://i.ytimg.com/vi/1PnVor36_40/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLAvN_gUkonX_alACtoe7wG14-_L5w" style="width: 100%; height: 100%;">
+                    </a>
+                    <h2 class="text-white"><b>css learn</b></h2> 
+                    <p class="text-white">lorem ipsum</p>        
+                </div>
+            </div>
+
+              <div class="col-sm-12 col-xs-12 col-md-4 portfolio-thumbnail ">
+                <div class="inner2" id="card">
+                    <a href="/html&css">
+                        <img src="https://i.ytimg.com/vi/mU6anWqZJcc/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLAC9NE3V6MUSXUt8x29wnVIYhE0LA" style="width: 100%; height: 100%;">
+                     </a>
+                    <h2 class="text-white"><b>html&css full course</b></h2> 
+                    <p class="text-white">lorem ipsum</p>        
+                </div>
+            </div>
+
+              <div class="col-sm-12 col-xs-12 col-md-4 portfolio-thumbnail ">
+                <div class="inner2" id="card">
+                    <a href="/phpintro">
+                        <img src="https://i.ytimg.com/vi/OK_JCtrrv-c/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBaBrtaxkpLhXxVBHE9e70I0saBSA" style="width: 100%; height: 100%;">
+                    </a>
+                    <h2 class="text-white"><b>php intro </b></h2> 
+                    <p class="text-white">lorem ipsum</p>        
+                </div>
+            </div>
+
+
+
+             <div class="col-sm-12 col-xs-12 col-md-4 portfolio-thumbnail ">
+                <div class="inner2" id="card">
+                    <a href="/phpsyntax">
+                        <img src="https://i.ytimg.com/vi/U10yvfIStx8/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBJOOubzBGJVKM6fv5hWsQp7XEGlw" style="width: 100%; height: 100%;">
+                    </a>
+                    <h2 class="text-white"><b>php syntax</b></h2> 
+                    <p class="text-white">lorem ipsum</p>        
+                </div>
+            </div>
+
+
+            <div class="col-sm-12 col-xs-12 col-md-4 portfolio-thumbnail ">
+                <div class="inner2" id="card">
+                    <a href="/phpinclude">
+                        <img src="https://i.ytimg.com/vi/fMTvi3Rys-o/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLB0oED0E5fkzmAsZ6eH5yd1-hpyCg" style="width: 100%; height: 100%;">
+                    </a>
+                    <h2 class="text-white"><b>php include</b></h2> 
+                    <p class="text-white">lorem ipsum</p>        
+                </div>
+            </div>
+            
+            <h1 class="javascript"></h1>
+            <div class="col-sm-12 col-xs-12 col-md-4 portfolio-thumbnail ">
+                <div class="inner2" id="card">
+                    <a href="/jsfundamentals">
+                         <img src="https://i.ytimg.com/vi/vEROU2XtPR8/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDTZcrG1hqreOkiGhHxj8_exMY5QA" style="width: 100%; height: 100%;">
+                     </a>
+                    <h2 class="text-white"><b>js fundamentals</b></h2> 
+                    <p class="text-white">lorem ipsum</p>        
+                </div>
+            </div>
+
+            <div class="col-sm-12 col-xs-12 col-md-4 portfolio-thumbnail ">
+                <div class="inner2" id="card">
+                    <a href="/jsvariables">
+                     <img src="https://i.ytimg.com/vi/edlFjlzxkSI/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLDKc0UBXRuI9xhXO4z5sRMTLGGc8A" style="width: 100%; height: 100%;">
+                     </a>
+                    <h2 class="text-white"><b>js variables</b></h2> 
+                    <p class="text-white">lorem ipsum</p>        
+                </div>
+            </div>
+
+
+            <div class="col-sm-12 col-xs-12 col-md-4 portfolio-thumbnail ">
+                <div class="inner2" id="card">
+                    <a href="/jsdatatypes">
+                <img src="https://i.ytimg.com/vi/O9by2KcR2v4/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBsHijj7g-l_zjU25X2AnabhNt24w" style="width: 100%; height: 100%;">
+            </a>
+                    <h2 class="text-white"><b>js datatypes</b></h2> 
+                    <p class="text-white">lorem ipsum</p>        
+                </div>
+            </div>
+         </div>
+    </div>
+</div>
+
 
 <?php include "includes/footer.view.php" ?>
 
 </body>
 </html>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+    $("#myinput").on("keyup",function(){
+    var value = $(this).val().toLowerCase();
+    $("#card div").filter(function(){
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+
+    });
+  });
+    });
+</script>
--- Website/views/includes/footer.view.php
+++ Website/views/includes/footer.view.php
@@ -1,2 +1,1 @@
 <script src="../asset/js/javascript.js"></script>
-
