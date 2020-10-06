ec65597165ab74bdbc7470fd7df41d57c11d1691
Alex <AlexSteen303@gmail.com>
2020-10-06 16:19:16 +0200

    Verboden toegang niet-ingelogde accounts voor courses.

--- Website/controllers/CourseController.php
+++ Website/controllers/CourseController.php
@@ -1,6 +1,10 @@
 <?php
 
-
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+    echo "Welkom " . $_SESSION['username'] . "!";
+} else {
+    echo "<HTML><a href='/login'>Log in om deze pagina te zien.</a></HTML>";
+}
 
 class CourseController 
 {
