88898333e3c4720d30829590823f524666be1107
Alex <AlexSteen303@gmail.com>
2020-10-06 19:24:23 +0200

    Aangepast block, minimale code toevoeging. Inloggen om access te krijgen tot WFFLIX content.

--- Website/controllers/CsslearnController.php
+++ Website/controllers/CsslearnController.php
@@ -1,5 +1,9 @@
 <?php
-
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+    echo "Welkom " . $_SESSION['username'] . "!";
+} else {
+    echo "<HTML><a href='/login'>Log in om deze pagina te zien.</a></HTML>";
+}
 
 class CsslearnController
 {
--- Website/controllers/JsdatatypesController.php
+++ Website/controllers/JsdatatypesController.php
@@ -1,8 +1,9 @@
 <?php
-/**
- * Created by: Stephan Hoeksema 2020
- * WFFlix-start
- */
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+    echo "Welkom " . $_SESSION['username'] . "!";
+} else {
+    echo "<HTML><a href='/login'>Log in om deze pagina te zien.</a></HTML>";
+}
 
 class JsdatatypesController
 {
