a33a8aade48cd040c8b98088b7bfab24034b7ab2
Alex <AlexSteen303@gmail.com>
2020-10-12 12:40:45 +0200

    Nav bugfix.

--- Website/views/includes/nav.view.php
+++ Website/views/includes/nav.view.php
@@ -1,4 +1,4 @@
-ert<div class="nav-wrapper">
+<div class="nav-wrapper">
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12 col-xs-12 col-md-12 col-sm-12 col-lg-12">
