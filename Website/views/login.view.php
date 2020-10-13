12da000db01321e28eb66d99830462a62fa8e22f
YektaU <yekta97@live.nl>
2020-10-13 15:46:28 +0200

    Tijdelijke fix voor submit zonder gegevens

--- Website/views/login.view.php
+++ Website/views/login.view.php
@@ -5,21 +5,35 @@
 <body>
 <?php include "includes/nav.view.php" ?>
 <section class="body">
+    <script>
+        function inputCheckLogin(){
+            var name = document.getElementById("name").value;
+            var password = document.getElementById("password").value;
+            var empty = ""
+
+            if(name == empty || password == empty){
+                alert("Vul een gebruikersnaam of wachtwoord in");
+                event.preventDefault();
+            }else{
+                return true;
+            }
+        }
+    </script>
     <div class="col-md-6">
     <div class="wrapper">
         <h2>Inloggen</h2>
         <p>Vul uw gegevens in om in te loggen.</p>
-        <form action="/login" method="post">
+        <form action="/login" method="post" onsubmit="inputCheckLogin()">
             <div class="error" style="color: #ff0000; font-size: 22px;">
                 <?= isset($_SESSION["login_incorrect"]) ? $_SESSION["login_incorrect"] : ''; ?>
             </div>
             <div class="form-group <?= isset($_SESSION["login_incorrect"]) ? 'error' : ''; ?>">
                 <label>Gebruikersnaam</label>
-                <input type="text" name="username" class="form-row" value="<?= isset($_POST["username"]) ? $_POST["username"] : ""?>">
+                <input type="text" name="username" class="form-row" id="name" value="<?= isset($_POST["username"]) ? $_POST["username"] : ""?>">
             </div>
             <div class="form-group">
                 <label>Wachtwoord</label>
-                <input type="password" name="password" class="form-row">
+                <input type="password" name="password" class="form-row" id="password">
             </div>
             <div class="form-group">
                 <input type="submit" name="submit" class="btn btn-primary" value="Login">
