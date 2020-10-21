4706c12574d7b49cb66ee53492f9da7187f98163
lvvcolin <lorenzo8399@hotmail.com>
2020-10-21 23:04:48 +0200

    home,login,about

--- Website/views/about.view.php
+++ Website/views/about.view.php
@@ -9,27 +9,37 @@ include "includes/head.view.php"
 
 <body>
 <?php include "includes/nav.view.php" ?>
-
 <div class="wrapper">
-    <div class="container-fluid" style="color: #FFCB05; font-family:'Calibri';">
-        <h1>Over ons:</h1>
-        <br>
-        <p class="text" style="font-size: larger">
-
-            <br>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-            <br>Labore et dolore magna aliquyam erat, sed diam voluptua.
-            <br>Et ea rebum. Stet clita kasd gubergren
-            <br>Ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-            <br>Sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
-            <br>Gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-            <br>Lorem ipsum sed diam nonumy eirmod tempor invidunt ut labore et
-            <br>Dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-            <br>Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-            <br>Forem ipsum dolor sit ametinvidunt ut labore
-
-        </p>
+<div class="w3-container w3-center w3-animate-left text-white">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <div class="zebi text-b">
+                    <h1 >JOIN THE MILLIONS LEARNING<br>
+                        TO CODE WITH WFFLIX.
+                    </h1>
+                </div>
+            </div>
+            <div class="col-md-12 text-center">
+                <div class="gezeik text-b">
+                    <p style="font-size: 20px;">
+                    <br>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                    <br>Labore et dolore magna aliquyam erat, sed diam voluptua.
+                    <br>Et ea rebum. Stet clita kasd gubergren
+                    <br>Ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+                    <br>Sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
+                     <br>Gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                    <br>Lorem ipsum sed diam nonumy eirmod tempor invidunt ut labore et
+                     <br>Dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+                    <br>Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                    <br>Forem ipsum dolor sit ametinvidunt ut labore
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
+</div>       
+</div>   
 
 <?php include "includes/footer.view.php" ?>
 
