a4b9d441c8230098d5b8190c0015518ea514fa94
lvvcolin <lorenzo8399@hotmail.com>
2020-10-24 19:24:22 +0200

    home

--- Website/views/home.view.php
+++ Website/views/home.view.php
@@ -8,39 +8,40 @@ include "includes/head.view.php"
 
 
 <body>
+
 <?php include "includes/nav.view.php" ?>
 
 
 <?php  if (isset($_SESSION["loggedIn"]) && $_SESSION["loggedIn"]) { ?>
-<div class="wrapper">
-    <div class="w3-container w3-center w3-animate-left text-white">
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                    <div class="zebi text-b">
-                        <h1 >WELCOME <?php echo $_SESSION['naam']; ?><br> 
-                            TO CODE WITH WFFLIX
-                        </h1>
+    <div class="wrapper">
+        <div class="w3-container w3-center w3-animate-left text-white">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                        <div class="zebi text-b">
+                            <h1 >WELCOME <?php echo $_SESSION['naam']; ?><br> 
+                                TO CODE WITH WFFLIX
+                            </h1>
+                        </div>
                     </div>
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                    <div class="gezeik text-b">
-                        <p>
-                            Develop more than skills. Give your team the<br>
-                            knowledge, experience, and confidence <br>
-                            they need to tackle any problem.
-                        </p>
+                    <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                        <div class="gezeik text-b">
+                            <p>
+                                Develop more than skills. Give your team the<br>
+                                knowledge, experience, and confidence <br>
+                                they need to tackle any problem.
+                            </p>
+                        </div>
                     </div>
+                    
                 </div>
-                
             </div>
         </div>
     </div>
-</div>
 
 
  <?php }else { ?>
-<div class="wrapper">
+    <div class="wrapper">
     <div class="w3-container w3-center w3-animate-left text-white">
         <div class="container">
             <div class="row">
@@ -67,52 +68,49 @@ include "includes/head.view.php"
             </div>
         </div>
     </div>
-</div>
-<div class="space">
-</div>
+    </div>
+    <div class="space">
+    </div>
 
 
 
 
-<div class="card bg-dark text-white">
-  <img class="card-img" src="../asset/img/apple_imac_free_mockup.png" alt="Card image">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 no-padding">
+                <div class="card">
+                  <img class="" src="../asset/img/apple_imac_free_mockup.png" alt="Card image">
 
-      <div class="card-img-overlay" id="wrapper2">
-        <div class="inner-wrapper">
-            <div class="row">
-                <div class="col-md-12 text-center" >
-                    <div class="row">
-                        <div class="col-md-12">
-                            <h2 class="card-title">It’s time to start
-                                investing in yourself
-                            </h2>   
-                        </div>
-                
-                        <div class="col-md-12" >
-                            <span>Because learning to code might be<br> the easiest way to change your career.
-                            </span>
-                        </div>
-                        <div class="col-md-12">
+                      <div  id="wrapper2">
+                        <div class="inner-wrapper">
+                            <div class="row">
+                                <div class="col-md-12 text-center" >
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <h2>It’s time to start
+                                                investing in yourself
+                                            </h2>   
+                                        </div>
+                                
+                                        <div class="col-md-12" >
+                                            <span>Because learning to code might be<br> the easiest way to change your career.
+                                            </span>
+                                        </div>
+                                        <div class="col-md-12">
 
-                         <a href="/register" type="button" class="btn btn-outline-success btn-lg" id="space-button1">register!</a>
+                                         <a href="/register" type="button" class="btn btn-outline-success btn-lg" id="space-button1">register!</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
-
-
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-
-        </div>
-    </div>
-</div>
-
+    
 
 <?php } ?>
 
