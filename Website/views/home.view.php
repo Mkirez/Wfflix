d45a27807c542b0f2e67001b02d43211e7016300
lvvcolin <lorenzo8399@hotmail.com>
2020-10-24 15:23:05 +0200

    home pagina

--- Website/views/home.view.php
+++ Website/views/home.view.php
@@ -17,14 +17,14 @@ include "includes/head.view.php"
     <div class="w3-container w3-center w3-animate-left text-white">
         <div class="container">
             <div class="row">
-                <div class="col-md-12 text-center">
+                <div class="col-xs-12 col-sm-12 col-md-12 text-center">
                     <div class="zebi text-b">
                         <h1 >WELCOME <?php echo $_SESSION['naam']; ?><br> 
                             TO CODE WITH WFFLIX
                         </h1>
                     </div>
                 </div>
-                <div class="col-md-12 text-center">
+                <div class="col-xs-12 col-sm-12 col-md-12 text-center">
                     <div class="gezeik text-b">
                         <p>
                             Develop more than skills. Give your team the<br>
@@ -38,31 +38,65 @@ include "includes/head.view.php"
         </div>
     </div>
 </div>
-<div class="container-fluid">
 
-</div>
 
- <?php }else{ ?>
+ <?php }else { ?>
 <div class="wrapper">
     <div class="w3-container w3-center w3-animate-left text-white">
         <div class="container">
             <div class="row">
-                <div class="col-md-12 text-center">
+                <div class="col-xs-12 col-sm-12 col-md-12 text-center">
                     <div class="zebi text-b">
                         <h1 >JOIN THE MILLIONS LEARNING<br>
                             TO CODE WITH WFFLIX
                         </h1>
                     </div>
                 </div>
-                <div class="col-md-12 text-center">
+                <div class="col-xs-12 col-sm-12 col-md-12 text-center">
                     <div class="gezeik text-b">
                         <p>
                             Develop more than skills. Give your team the<br>
                             knowledge, experience, and confidence <br>
                             they need to tackle any problem.
                         </p>
-                        <a href="/register" type="button" class="btn btn-success btn-lg">register</a>
+                    
+                    </div>
+                    <a href="#wrapper2">
+                        <i aria-hidden="true" class="fa fa-arrow-down scroll"></i>
+                    </a>
+                </div>          
+            </div>
+        </div>
+    </div>
+</div>
+<div class="space">
+</div>
 
+
+
+
+<div class="card bg-dark text-white">
+  <img class="card-img" src="../asset/img/Inkedapple_imac_free_mockup_LI.jpg" alt="Card image">
+
+      <div class="card-img-overlay" id="wrapper2">
+        <div class="inner-wrapper">
+            <div class="row">
+                <div class="col-md-12 text-center" >
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h2 class="card-title">It’s time to start
+                                investing in yourself
+                            </h2>   
+                        </div>
+                
+                        <div class="col-md-12" >
+                            <span>Because learning to code might be<br> the easiest way to change your career.
+                            </span>
+                        </div>
+                        <div class="col-md-12">
+
+                         <a href="/register" type="button" class="btn btn-outline-success btn-lg" id="space-button1">register!</a>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -70,10 +104,6 @@ include "includes/head.view.php"
     </div>
 </div>
 
-
-<div class="space">
- 
-</div>
 
 <?php } ?>
 
